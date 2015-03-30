fbda11be890a18a5048f20d72d4fc58756a038d3
Mark Nijhof <mark.nijhof@cre8ivethought.com>
2015-03-30 13:27:23 +0200

    fixed a bug when having multiple ws instances running

--- ws.sh
+++ ws.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env escript
 %% -*- erlang -*-
-%%! -smp enable -sname ws -mnesia debug verbose
 
 main([String, Path]) ->
     try
         Port = list_to_integer(String),
-        serve(Port, Path)
+        Name = "ws" ++ String,
+        serve(Name, Port, Path)
     catch
         A:B ->
             io:format("Error: ~p:~p~n", [A,B]),
@@ -18,8 +18,7 @@ usage() ->
     io:format("usage: ws <port:8080> <path:.>~n"),
     halt(1).
 
-serve(Port, Path) ->
-    io:format("started listening on ~p serving ~p~n", [Port, Path]),
+serve(Name, Port, Path) ->
     inets:start(),
 
     MimeTypes = [{"html","text/html"},
@@ -35,14 +34,15 @@ serve(Port, Path) ->
 
     {ok, _Pid} = inets:start(httpd,
         [{port, Port},
-         {server_name,"ws"},
+         {server_name, Name},
          {server_root, Path},
          {document_root, Path},
          {bind_address, {127,0,0,1}},
          {directory_index, ["index.html", "index.htm"]},
-         {mime_types,MimeTypes}
+         {mime_types, MimeTypes}
         ]),
 
+    io:format("started listening on ~p serving ~p~n", [Port, Path]),
     io:format("supported mimetypes:~n~p~n", [MimeTypes]),
 
     receive
